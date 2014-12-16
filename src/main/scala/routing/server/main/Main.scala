641a132b7d47441f715decd6a0af9b67af88badf
unknown <bhavneshgugnani>
2014-12-15 22:50:32 -0500

    user registration and tweet service running for routing server

--- src/main/scala/routing/server/main/Main.scala
+++ src/main/scala/routing/server/main/Main.scala
@@ -8,12 +8,13 @@ import spray.routing.authentication.BasicAuth
 import spray.routing.directives.CachingDirectives._
 import spray.httpx.encoding._
 import spray.http.MediaTypes
-import main.scala.common.Constants
+import main.scala.common._
 import java.util.concurrent.ConcurrentHashMap
 import scala.collection.concurrent
 import akka.actor.ActorRef
 import scala.collection.convert.decorateAsScala.mapAsScalaConcurrentMapConverter
 import main.scala.common.RegisterUser
+import com.typesafe.config.ConfigFactory
 
 object Main extends App with SimpleRoutingApp {
 
@@ -24,7 +25,22 @@ object Main extends App with SimpleRoutingApp {
   //val simpleCache = routeCache(maxCapacity = 1000, timeToIdle = Duration("30 min"))
   val localAddress: String = java.net.InetAddress.getLocalHost.getHostAddress()
   val routingServerPort: Int = 8090
-  implicit val system = ActorSystem("RoutingServer")
+
+  val configString = """akka {
+  actor {
+    provider = "akka.remote.RemoteActorRefProvider"
+  }
+  remote {
+    enabled-transports = ["akka.remote.netty.tcp"]
+    netty.tcp {
+      hostname = """ + localAddress + """
+      port = """ + (routingServerPort + 1).toInt + """
+    }
+ }
+}"""
+
+  val configuration = ConfigFactory.parseString(configString)
+  implicit val system = ActorSystem("RoutingServer", ConfigFactory.load(configuration))
 
   val list: List[Int] = List()
 
@@ -38,39 +54,55 @@ object Main extends App with SimpleRoutingApp {
       }
     } ~
       get {
-        path("hello" / IntNumber) { index =>
+        path("hello" / Segment) { index =>
           complete {
+            println(index)
             "Hello" + index
           }
         }
       } ~
+      //Register user
+      //http://172.16.110.167:8090/registeruser/bhavnesh
       post {
         path("registeruser" / Segment) { userName =>
+          var uuid = java.util.UUID.randomUUID().toString()
+          val remote = system.actorSelection(akkaServerPath + "UserRegistrationRouter")
+          remote ! RegisterUser(uuid, userName, "")
           complete {
-            var done = false
-            var uuid: String = ""
-            while (!done) {
-              uuid = java.util.UUID.randomUUID().toString()
-              if (requestMap.get(uuid) == None) {
-                //requestMap += uuid -> sender
-                done = true
-              }
-            }
-            val remote = system.actorSelection(akkaServerPath + "UserRegistrationRouter")
-            remote ! RegisterUser(uuid, userName, "")
+            println("Done")
             "Done"
           }
         }
-      }
-    /*get{
-      path("hello" / "give"){
-        respondWithMediaType(MediaTypes.'application/json'){
-          complete{
-            "List"
+      } ~
+      //Register multiple users
+      //http://172.16.110.167:8090/userregistration?ip=1.2.3.4&client=100&samplesize=10&peakactorname=PeakActor&peakactorfollowerscount=10
+      post {
+        path("userregistration") {
+          parameters("ip".as[String], "clients".as[Int], "samplesize".as[Int], "peakactorname".as[String], "peakactorfollowerscount".as[Int]) { (ip, clients, samplesize, peakactorname, peakactorfollowerscount) =>
+            var uuid = java.util.UUID.randomUUID().toString()
+            val remote = system.actorSelection(akkaServerPath + "UserRegistrationRouter")
+            remote ! RegisterUsers(uuid, ip, clients, "", constants.followers, samplesize, peakactorname, peakactorfollowerscount)
+            complete {
+              "Done"
+            }
+          }
+        }
+      } ~
+      //Post Tweet Update
+      //http://172.16.110.167:8090/tweet/update/bhavnesh?tweet=abcd
+      post {
+        path("tweet" / "update" / Segment) { username =>
+          parameters("tweet".as[String]) { (tweet) =>
+            var uuid = java.util.UUID.randomUUID().toString()
+            val endPoint = "postupdate"
+            val remote = system.actorSelection(akkaServerPath + "UserRegistrationRouter")
+            remote ! new AkkaRequest(uuid, "", endPoint, username, "", tweet)
+            complete {
+              "Done"
+            }
           }
         }
       }
-    }*/
   }
 
   /*startServer(interface = localAddress , port = routingServerPort) {
