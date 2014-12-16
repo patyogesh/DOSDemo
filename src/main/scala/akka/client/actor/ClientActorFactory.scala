e1bf3231553b6c9964170c27e9c1c25c16805ab6
unknown <bhavneshgugnani>
2014-12-16 12:09:35 -0500

    Making akka client asynchronous

--- src/main/scala/akka/client/actor/ClientActorFactory.scala
+++ src/main/scala/akka/client/actor/ClientActorFactory.scala
@@ -6,6 +6,7 @@ import akka.actor.ActorRef
 import akka.actor.Props
 import akka.actor.actorRef2Scala
 import main.scala.common.Start
+import scala.concurrent.duration.DurationDouble
 
 //#This class creates client actors to simulate users on client side.
 class ClientActorFactory(clients: Int, serverAddress: String, followers: Array[Int], sampleSize: Int, numberOfTweetsPerDay: Array[Int], offset: Double, localAddress: String, timeMultiplier: Double, peakActor: ActorRef) extends Actor {
@@ -14,6 +15,9 @@ class ClientActorFactory(clients: Int, serverAddress: String, followers: Array[I
   for (i <- 0 to clients - 1) {
     clientActors += context.system.actorOf(Props(new ClientActor(serverAddress, followers((i % sampleSize)), numberOfTweetsPerDay((i % sampleSize)), i * offset, "Client" + i + "@" + localAddress, clients, timeMultiplier)), "Client" + i + "@" + localAddress)
   }
+  
+  import context.dispatcher
+  val tweet = context.system.scheduler.scheduleOnce(5000 milliseconds, self, Start)
 
   def receive = {
       case Start(requestUUID : String) =>
